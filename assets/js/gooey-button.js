3c819dedd0399690a5b76ac325ebc03738332c5f
Tearless <90409549+tearpc@users.noreply.github.com>
2024-12-19 16:05:27 +0530

    Update gooey-button.js

--- assets/js/gooey-button.js
+++ assets/js/gooey-button.js
@@ -1,11 +1,1 @@
-// Gooey Button Script
-document.addEventListener('DOMContentLoaded', () => {
-  const gooeyButton = document.querySelector('.gooey-button svg');
 
-  if (gooeyButton) {
-    gooeyButton.addEventListener('click', () => {
-      console.log('Gooey Button Clicked!');
-      // Add custom behavior for the gooey button here
-    });
-  }
-});
